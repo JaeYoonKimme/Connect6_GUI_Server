0db51c8ad0eea23261e6d409df7556b3cc4d944e
eunice-nxn <medofch@gmail.com>
2021-09-22 15:57:08 +0900

    new client

--- client/Client.java
+++ client/Client.java
@@ -11,90 +11,98 @@ public class Client {
         try {
             Socket socket = new Socket("localhost", port);
             socket.setTcpNoDelay(true);
-			System.out.println("Connected to server");
+                        System.out.println("Connected to server");
             inputStream = socket.getInputStream();
             outputStream = socket.getOutputStream();
-                   
-		} catch (IOException e){
+
+                } catch (IOException e){
             System.err.println("catch" + e);
         }
     }
-	public void recvRedStones(){
-		try{
-			int sizeOfRedStones = inputStream.read();
-			byte[] str_byte = new byte[sizeOfRedStones];
-			inputStream.read(str_byte, 0, sizeOfRedStones);
-			String redinfo = new String(str_byte);
-			System.out.println("Got Red Stone ->" + redinfo);
-		
-		}catch (IOException e) {
-			System.err.println("recv redstones" + e);
-		}
-	}
+        public void recvRedStones(){
+                try{
+                        int sizeOfRedStones = inputStream.read();
+                        byte[] str_byte = new byte[sizeOfRedStones];
+                        inputStream.read(str_byte, 0, sizeOfRedStones);
+                        String redinfo = new String(str_byte);
+                        System.out.println("Got Red Stone ->" + redinfo);
 
-	public int byteToInt(byte[] bytes) {
+                }catch (IOException e) {
+                        System.err.println("recv redstones" + e);
+                }
+        }
+
+        public int byteToInt(byte[] bytes) {
              return ((bytes[0] & 0xFF) << 24) |
                     ((bytes[1] & 0xFF) << 16) |
                     ((bytes[2] & 0xFF) << 8 ) |
                     ((bytes[3] & 0xFF) << 0 );
-	}
+        }
 
-	public byte[] intToByte(int intValue) {
-		byte[] byteArray = new byte[4];
-		byteArray[0] = (byte)(intValue >> 24);
-		byteArray[1] = (byte)(intValue >> 16);
-		byteArray[2] = (byte)(intValue >> 8);
-		byteArray[3] = (byte)(intValue);
-		return byteArray;
-	}
-
-
-	public void echo() {
-		try {
-			while(true) {
-				Scanner scanner = new Scanner(System.in);
-				String messageToSend = scanner.next();
-			
-				ByteBuffer b = ByteBuffer.allocate(4);
-				byte[] sizeByteToSend = intToByte(messageToSend.length());
-				outputStream.write(sizeByteToSend);
-			
-				byte[] messageByteToSend = messageToSend.getBytes();
-				outputStream.write(messageByteToSend);
+        public byte[] intToByte(int intValue) {
+                byte[] byteArray = new byte[4];
+                byteArray[0] = (byte)(intValue >> 24);
+                byteArray[1] = (byte)(intValue >> 16);
+                byteArray[2] = (byte)(intValue >> 8);
+                byteArray[3] = (byte)(intValue);
+                return byteArray;
+        }
 
 
+        public void echo() {
+                try {
+                        while(true) {
+                                Scanner scanner = new Scanner(System.in);
+                                String messageToSend = scanner.next();
 
-				byte[] sizeByteToRecv = new byte[4];
-				inputStream.read(sizeByteToRecv, 0, 4);
-				int sizeToRecv = byteToInt(sizeByteToRecv);
+                                ByteBuffer b = ByteBuffer.allocate(4);
+                                byte[] sizeByteToSend = intToByte(messageToSend.length());
+                                outputStream.write(sizeByteToSend);
 
-				System.out.println("Echo message size from Server :"+sizeToRecv);
+                                byte[] messageByteToSend = messageToSend.getBytes();
+                                outputStream.write(messageByteToSend);
 
-				byte[] messageByteToRecv = new byte[sizeToRecv];
-				inputStream.read(messageByteToRecv, 0, sizeToRecv);
-				String message = new String(messageByteToRecv);
 
-				System.out.println("Echo from Server : "+message);
-			}
-		} catch (IOException e){
-			System.err.println("IOException e");
-		}
-	}
 
-	public void sendStones() {
-		Scanner s = new Scanner(System.in);
-		while(true) {
-			String stones = s.nextLine();
-			int sizeOfStones = stones.length();
-			try {
-				outputStream.write(sizeOfStones);
-				outputStream.write(stones.getBytes(), 0, sizeOfStones);
-			} catch (IOException e) {
-				System.err.println("Send redStone" + e);
-			}
+                                byte[] sizeByteToRecv = new byte[4];
+                                inputStream.read(sizeByteToRecv, 0, 4);
+                                int sizeToRecv = byteToInt(sizeByteToRecv);
 
-		}
-	}
+                                System.out.println("Echo message size from Server :"+sizeToRecv);
+
+                                byte[] messageByteToRecv = new byte[sizeToRecv];
+                                inputStream.read(messageByteToRecv, 0, sizeToRecv);
+                                String message = new String(messageByteToRecv);
+
+                                System.out.println("Echo from Server : "+message);
+                        }
+                } catch (IOException e){
+                        System.err.println("IOException e");
+                }
+        }
+
+        public void sendStones() {
+                Scanner s = new Scanner(System.in);
+                        String stones = s.nextLine();
+                        int sizeOfStones = stones.length();
+                        try {
+                                outputStream.write(sizeOfStones);
+                                outputStream.write(stones.getBytes(), 0, sizeOfStones);
+                        } catch (IOException e) {
+                                System.err.println("Send redStone" + e);
+                        }
+
+        }
+
+        public void recvStones() {
+            try {
+                int sizeOfStones = inputStream.read();
+                byte[] bytesOfStones = new byte[sizeOfStones];
+                inputStream.read(bytesOfStones, 0, sizeOfStones); 
+                String stones = new String(bytesOfStones);
+                System.out.println("Receive stones " + stones);
+            } catch (IOException e) {}
+        }
 
 
     public static void main(String[] args) {
@@ -103,25 +111,17 @@ public class Client {
         int color = sc.nextInt();
         int port = sc.nextInt();
         c.init(color, port);
- 		//c.echo();
-		c.recvRedStones();
-		c.sendStones();
-	}
+                //c.echo();
+        c.recvRedStones();
+        if(color == 0) {
+            c.sendStones();
+            c.recvStones();
+        } else {
+            c.recvStones();
+        }
+        while(true){
+            c.sendStones();
+            c.recvStones();
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
